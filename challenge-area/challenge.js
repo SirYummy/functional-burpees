1b190147f427e01e5465ca07007ef948f02ff931
SirYummy <micah.bolen@gmail.com>
2018-01-24 15:00:08 -0600

    chore: Do functional burpees.

--- challenge-area/challenge.js
+++ challenge-area/challenge.js
@@ -1,1 +1,84 @@
 //Good luck! Maybe start by making that fetch request ;)
+const fetchQuestions = async () => {
+    const questions = await(await fetch('https://opentdb.com/api.php?amount=10&category=18')).json()
+    return questions
+}
+
+let triviaQuestions = []
+let fixedQuestions = []
+let easyQuestions = []
+let sortedQuestions = []
+let difficultyAggregate = {easy:0,medium:0,hard:0}
+
+fetchQuestions().
+    then(questionsResponse => triviaQuestions = questionsResponse.results).
+    then(foo => {
+        fixHtmlEntities(triviaQuestions)    
+        filterNonEasyQuestions(triviaQuestions)
+        sortQuestionsByDifficulty(triviaQuestions)
+        questionDifficultyCount(triviaQuestions)
+        isEveryQuestionCategoryComputerScience(triviaQuestions)
+        grabOnlyMediumQuestions(triviaQuestions)
+    }).
+    catch(e => console.log(e))
+
+const fixHtmlEntities = (triviaQuestions) => {
+    fixedQuestions = triviaQuestions.map((question) => {
+        question.question = question.question.replace(/&#(\d+);/g, (match, dec) => {
+            return String.fromCharCode(dec);
+        })
+        return question
+    })
+    console.log('Fixed questions: ', fixedQuestions)
+}
+
+const filterNonEasyQuestions = (questions) => {
+    easyQuestions = questions.filter(question => question.difficulty === 'easy')
+    console.log("Easy questions: ", easyQuestions)
+}
+
+const sortQuestionsByDifficulty = (questions) => {
+    sortedQuestions = questions.sort((questionA, questionB) => questionA.difficulty > questionB.difficulty)
+    console.log("Questions sorted by difficulty: ", sortedQuestions)
+}
+
+const questionDifficultyCount = (questions) => {
+    let answer = questions.reduce((aggregator = {easy:0,medium:0,hard:0}, currentQuestion) => {
+        switch (currentQuestion.difficulty) {
+            case 'easy':
+                ++aggregator.easy
+                break
+            case 'medium':
+                ++aggregator.medium
+                break
+            case 'hard':
+                ++aggregator.hard
+                break
+            default:
+                break
+        }
+        return aggregator
+    },{easy:0,medium:0,hard:0})
+    console.log("Difficulty aggregate: ", answer)
+}
+
+const isEveryQuestionCategoryComputerScience = (questions) => {
+    let answer = questions.every(question => question.category === "Science: Computers")
+    console.log("Is every question category Computer Science?", answer ? 'yes' : 'no')
+}
+
+const grabOnlyMediumQuestions = (questions) => {
+    let answer = sortByType(
+        questions.filter(
+            (question) => {
+                return question.difficulty === "medium"
+            }
+        )
+    )
+    console.log("Only medium questions sorted by type: ", answer)
+}
+
+const sortByType = (questions) => {
+    let sortedQuestions = questions.sort((questionA, questionB) => questionA.type > questionB.type)
+    console.log("Questions sorted by type: ", sortedQuestions)
+}
--- primer.js
+++ primer.js
@@ -3,11 +3,59 @@
 const magicNumbers = [72, 3, 9, 10, 65, 0, 18, 21];
 
 // Find the largest number from the magicNumbers array => 72
+const largestReducer = (accumulator, current) => {
+    return current > accumulator ? current : accumulator
+}
+const largest = magicNumbers.reduce(largestReducer, magicNumbers[0])
+
+console.log("Largest number: ", largest)
 
 /*
    Sort the numbers from highest to lowest, but leave the original array the same
    [ 72, 65, 21, 18, 10, 9, 3, 0 ]
 */
+const quickSort = (arr, left, right) => {
+    const len = arr.length;
+    let pivot;
+    let partitionIndex;
+
+    if(left < right){
+        pivot = right;
+        partitionIndex = partition(arr, pivot, left, right);
+        
+        //sort left and right
+        quickSort(arr, left, partitionIndex - 1);
+        quickSort(arr, partitionIndex + 1, right);
+    }
+    return arr;
+}
+
+const partition = (arr, pivot, left, right) => {
+    const pivotValue = arr[pivot];
+    let partitionIndex = left;
+ 
+    for(var i = left; i < right; i++){
+     if(arr[i] < pivotValue){
+       swap(arr, i, partitionIndex);
+       partitionIndex++;
+     }
+   }
+   swap(arr, right, partitionIndex);
+   return partitionIndex;
+}
+
+const swap = (arr, i, j) => {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+ }
+
+const sorted = 
+    quickSort(magicNumbers, 0, 11).
+    filter((value) => typeof value !== 'undefined').
+    reverse();
+
+console.log("Sorted: ", sorted)
 
 const names = ['Michael', 'Ying', 'Sid', 'Ravi', 'Rodo', 'Mark'];
 
@@ -16,6 +64,9 @@ const names = ['Michael', 'Ying', 'Sid', 'Ravi', 'Rodo', 'Mark'];
    {name:'Michael'}
 
 */
+const objectifyName = x => { return {name: x} }
+const objectifiedNames = Array.from(names, objectifyName)
+console.log("objectified names: ", objectifiedNames)
 
 const coolPeople = [
    {
@@ -40,7 +91,15 @@ const coolPeople = [
    }
 ];
 
+
+
 //filter the cool people so that it excludes anyone that has an 'M' in their name
+const excludeMs = (peep) => !peep.name.includes('M')
+const filteredPeepz = coolPeople.filter(excludeMs)
+console.log("Filtered Peepz: ", filteredPeepz);
 
 
 // How many years of programming does everyone have together?
+const aggregateReducer = (accumulator, currentValue) => accumulator + currentValue.yearsProgramming
+const aggregateYearsProgramming = coolPeople.reduce(aggregateReducer, 0);
+console.log("Aggregate years programming: ", aggregateYearsProgramming)
